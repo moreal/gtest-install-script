5330b01f71bf7288fc00ea64adff7a89be75d7ac
moreal <dev.moreal@gmail.com>
2018-09-23 08:35:58 +0900

    Add for Ubuntu

---
+++ gtest-install-for-ubuntu.sh
@@ -0,0 +1,18 @@
+function isInstalledPackage
+{
+    apt list --installed | grep $1
+}
+
+if ! isInstalledPackage cmake ; then
+    sudo apt install cmake
+fi
+
+if ! isInstalledPackage libgtest-dev ; then
+    sudo apt install libgtest-dev
+fi
+
+cd /usr/src/gtest
+sudo cmake CMakeList.txt
+sudo make
+sudo cp libgtest.* /usr/lib
+sudo cp libgtest_main.* /usr/lib
